788d765c727df085922de77f9ec8ab780eae88fd
Mario Quintana <pyrros2000@gmail.com>
2018-10-01 14:26:58 -0700

    Form fields required and throws error

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
+import TextInputGroup from '../layout/TextInputGroup';
 import uuid from 'uuid';
 
 class AddContact extends Component {
@@ -7,30 +8,44 @@ class AddContact extends Component {
   state = {
     name: '',
     email: '',
-    phone: ''
+    phone: '',
+    errors: {}
   }
 
   onSubmit = (dispatch, e) => {
     e.preventDefault();
     const { name, email, phone } = this.state;
 
+    if(name === '') {
+      this.setState({ errors: { name: 'Name is required' }});
+      return;
+    }
+    if(email === '') {
+      this.setState({ errors: { email: 'Email is required' }});
+      return;
+    }
+    if(phone === '') {
+      this.setState({ errors: { phone: 'Phone is required' }});
+      return;
+    }
+
     const newContact = {
       id: uuid(),
       name,
       email,
-      phone
+      phone,
     }
 
     dispatch({ type: 'ADD_CONTACT', payload: newContact });
 
-    this.setState({ name: '', email: '', phone: '' });
+    this.setState({ name: '', email: '', phone: '', errors: {} });
 
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
 
     return (
       <Consumer>
@@ -41,18 +56,9 @@ class AddContact extends Component {
               <div className="card-header">Add Contact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" name="name" value={name} className="form-control form-control-lg"placeholder="Enter Name" onChange={this.onChange}/>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" value={email} className="form-control form-control-lg"placeholder="Enter Email" onChange={this.onChange}/>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="phone">Phone</label>
-                    <input type="text" name="phone" value={phone} className="form-control form-control-lg"placeholder="Enter Phone" onChange={this.onChange}/>
-                  </div>
+                  <TextInputGroup label="Name" name="name" placeholder="Enter Name" value={name} onChange={this.onChange} error={errors.name}/>
+                  <TextInputGroup label="Email" typen="email" name="email" placeholder="Enter Email" value={email} onChange={this.onChange} error={errors.email}/>
+                  <TextInputGroup label="Phone" name="phone" placeholder="Enter Phone" value={phone} onChange={this.onChange} error={errors.phone}/>
                   <input type="submit" value="Add Contact" className="btn btn-light btn-block"/>
                 </form>
               </div>
